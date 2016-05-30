d010be20031ea9f3ec2d613c06664f235d8d55ea
sathishkategaru <sathish.kategaru1@gmail.com>
2016-05-30 10:51:41 +0400

    optimizing for production build
    added plugins in webpack to make the production build

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,11 @@ module.exports = {
     },
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin()
     ],
     resolve: {
         extensions: ['', '.js', '.jsx']
